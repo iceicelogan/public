0806c5a655fb1c762147f514cb9aae860c8b3bf4
Logan Ice <clarkloganice@gmail.com>
2019-02-26 15:02:42 -0600

    Commenting E33
    Euler 33 solution, commented and cleaned up

--- Python/euler_solutions/Euler33.py
+++ Python/euler_solutions/Euler33.py
@@ -1,21 +1,50 @@
+################################
+#                              #
+#     Solution to Euler 33     #
+#  Digit Cancelling Fractions  #
+#                              #
+################################
+
+
+# import time for benchmarking and Fraction to simplify fractions
 import time
 from fractions import Fraction
 
+
+# Log the start time
 start = time.time()
 
+
+# Initialize two empty lists
 i_thing = []
 j_thing = []
-for i in range(10,99):
-    for j in range(10,99):
-        if (i/j)<1:
+
+
+# Loop through 10-99 89 times (as we know the numerator and denominator are both 10-99
+# If it's a proper fraction, append the numerator and denominator to i and j respectively
+for i in range(10, 99):
+    for j in range(10, 99):
+        if (i/j) < 1:
             i_thing.append(i)
             j_thing.append(j)
+
+
+# Store a variable for the length of i_thing so we reduce our function calls
 length = (len(i_thing))
 
+
+# Initialize variables so we don't explode the world with a division by 0
 top_sum = 1
 bottom_sum = 1
 count = 0
+
+
+# print some language to help us understand the output
 print("The Four Fractions Are: \n")
+
+
+# Loop through the cases of i_thing / j_thing, cancelling parts of the numbers.
+# Must loop through a couple cases, removing any case that would result in division by 0
 for i in range(int(length)):
     if int(str(j_thing[i])[1]) != 0 and int(str(j_thing[i])) != 0:
         if str(i_thing[i])[0] == str(j_thing[i])[0]:
@@ -42,10 +71,16 @@ for i in range(int(length)):
                 top_sum *= int(str(i_thing[i]))
                 bottom_sum *= int(str(j_thing[i]))
 
-print("Multiplied together, we get: "+str(top_sum)+" over "+str(bottom_sum)+"\n Which condenses to: "+str(Fraction(int(top_sum),int(bottom_sum))))
 
-#time at the end of program execution
+# With the hard work complete, print our top and bottom sums
+print("Multiplied together, we get: " +
+      str(top_sum) + " over "+str(bottom_sum) +
+      # Condense the fraction using the Fraction function
+      "\n Which condenses to: " +
+      str(Fraction(int(top_sum), int(bottom_sum))))
+
+# Log time at the end of program execution
 end = time.time()
 
-#Total time taken for the program execution
-print("And it took "+str(end - start)+" Seconds")
+# Print the total time taken for the program execution
+print("And it took "+str(round(end - start, 3))+" Seconds")
