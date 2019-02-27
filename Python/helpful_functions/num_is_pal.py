ab10114802403cf774e2c0066a07d62006dbf377
Logan Ice <clarkloganice@gmail.com>
2019-02-27 08:35:45 -0600

    Palindrom
    Commented up the palindrome code

--- Python/helpful_functions/num_is_pal.py
+++ Python/helpful_functions/num_is_pal.py
@@ -1,4 +1,15 @@
+##########################################################
+#                                                        #
+#   Short Function to determine if number is palindrome  #
+#                                                        #
+##########################################################
+
+
+# Function only takes one input, the number in question
 def num_is_pal(num):
+
+    # Reverse the string and do a boolean check
+    # If it's the same as the original, it's a palindrome!
     if str(num) == str(num)[::-1]:
         return True
     else:
