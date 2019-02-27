b8c31fc22d6486a8655db51bf98ad4bbb18bdb79
Logan Ice <clarkloganice@gmail.com>
2019-02-27 09:45:10 -0600

    Beer
    Fixed and commented on all of the beer files

--- R/helpful_functions/combine_all_csvs.R
+++ R/helpful_functions/combine_all_csvs.R
@@ -1,8 +1,21 @@
+####################################################
+#                                                  #
+# Function combines all csvs from a given location #
+#            Result is a single data set           #
+#                                                  #
+####################################################
+
+
+# Function takes one input, a string directory
+# User should assign this to a variable 
+# x <- combine_all_csvs()
 combine_all_csvs <- function(location){
   
   # Create list of each .csv file in a given location
   list_of_csvs <- list.files(path=location, pattern="*.csv")
   
+  # ListApply read.csv across all files in the location
+  # Then rbind them together
   do.call(rbind, lapply(
                   paste0(location, list.files(path = location)),
                   read.csv))
--- R/projects/beer/beer_advocate_ratings.R
+++ R/projects/beer/beer_advocate_ratings.R
@@ -1,14 +1,32 @@
+#############################################################
+#                                                           #
+#  Program to read csv and produce gif of associated plots  #
+#                                                           #
+#############################################################
+
+
+# Load requisite packages
+# dplyr = manipulation
+# ggplot2 = charts
 library(dplyr)
 library(ggplot2)
-df3 <- read.csv('/Users/logan.ice/Documents/python_code/df3.csv', stringsAsFactors = FALSE)
-df3$Number.of.Reviews <- as.numeric(gsub(",","", df3$Number.of.Reviews))
 
+
+# Read appropriate csv (change path to match your system)
+df3 <- read.csv('/Users/logan.ice/Documents/python_code/df3.csv', stringsAsFactors = FALSE)
+
+
+# Add a "rank" field, Ranking each beer by rating
 df3 <- arrange(df3, desc(Rating)) %>%
   mutate(rank = 247 - nrow(df3):1) 
 
 
+# Write a function to create a chart, given a max(rank)
 iterate_chart <- function(var){
   
+  
+  # Conditional logic to set the colors of each Category
+  # This is manually set. Could automate in future
   if(var <= 5){
     vals = c('#4b0c11')
   } else if(var <= 7){
@@ -19,7 +37,11 @@ iterate_chart <- function(var){
     vals = c('#c38e0d','white','#e9d76c', '#4b0c11')
   }
   
+  
+  # Plot data! Filter makes sure you only plot points up to the input rank
   ggplot(data = filter(df3, rank <= var), aes(x = ABV, y = Rating, color = Category)) +
+    # Add extra geom_point, 1 size larger than the colors
+    # This makes a nice looking border to the points
     geom_point(color = 'black', size = 3) +
     scale_colour_manual(values = vals) +
     geom_point(size = 2) +
@@ -42,12 +64,21 @@ iterate_chart <- function(var){
 }
 
 
+# Set a length variable to minimize function calls
+# Add 10 to give a buffer at the end. 
+# 10 could be changed if you want.
+len <- nrow(df3) + 10
 
-len <- nrow(df3)
 
-location<-"/Users/logan.ice/Documents/git/public/R/beer_pngs"
+# Define location (again, change your code as needed)
+location<-"/Users/logan.ice/Documents/git/public/R/projects/beer/beer_files"
 
-for (i in 1:(len+10)) {
+
+# Loop through all values of rank
+# Set p based on how far you've iterated
+# That's necessary because of ImageMagick being dumb:
+# It views `11.png` as occuring before `2.png` so we have to trick it
+for (i in 1:len) {
   p <- if(i <= 50){
     paste(replicate(i,"1"),collapse="")
   } else if(i <= 100) {
@@ -62,14 +93,27 @@ for (i in 1:(len+10)) {
     paste(replicate(i,"6"),collapse="")
   }
   
+  
+  # After that step, we create a png for each plot and save it to our given location
   file_path = paste0(location, "/plot-",p,".png")
   g<-iterate_chart(i)
   ggsave(file_path, g, width = 10, height = 8, units = "cm",scale=2) 
   
-  print(paste(i,"out of",nrow(df3)+10))
+  
+  # For clarity, print progress for the user
+  print(paste(i,"out of",len))
 }
 
+
+# Change working directory for ImageMagick
 setwd(location)
 
+
+# Use ImageMagick from terminal to convert all pngs into one gif
+# I used a delay of 4 ms for this one as there are 250 pngs to loop through
+# For a typical chart, I would use 30-50 though
 system("convert -delay 4 *.png beer_ratings.gif")
+
+
+# After conversion, remove all pngs from that directory, as we have our gif
 file.remove(list.files(pattern=".png"))
