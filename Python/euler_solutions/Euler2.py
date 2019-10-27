70ff8bdff49eb66f749bca8f670f1c5dc070cf2c
Logan Ice <clarkloganice@gmail.com>
2019-10-27 10:33:39 -0500

    Updated Euler2
    Euler 2 Updated for readability reasons

--- Python/euler_solutions/Euler2.py
+++ Python/euler_solutions/Euler2.py
@@ -7,14 +7,6 @@
 ################################
 
 
-# import time for benchmarking
-import time
-
-
-# Log the start time
-start = time.time()
-
-
 ###########################################
 #                                         #
 #  Function to return Fibonacci Sequence  #
@@ -28,40 +20,20 @@ start = time.time()
 def fib(max_num):
 
     # Initialize our variables to loop across
-    first = 1
+    summed_value = 0
+    i = 2
     prev = 1
-    holder = 0
-    lister = 0
 
     # Loop over the max_num entered
-    for i in range(0, max_num):
-
-        if holder == 0:
-
-            # Special Case for holder = 0
-            lister += first
-            holder = 1
-            print(i)
-
-        else:
-
-            # Add the number for a running sum here
-            lister += first
-            holder = first
-            first += prev
-            prev = holder
-            print(i)
+    while i < max_num:
+        if i % 2 == 0:
+            summed_value += i
+        holder = i
+        i += prev
+        prev = holder
 
     # return the answer
-    return lister
+    return summed_value
 
 
 print(fib(4000000))
-
-
-# Log time at the end of program execution
-end = time.time()
-
-
-# Print the total time taken for the program execution
-print("This problem took "+str(round(end - start, 10))+" Seconds")
